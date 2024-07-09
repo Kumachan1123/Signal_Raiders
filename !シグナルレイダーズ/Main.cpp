0dbd65004d5d585f096b7c660541fb971a31706a
Kumachan1123 <kumakei2004@gmail.com>
2024-07-09 11:35:41 +0900

    ウィンドウの名前を変更

--- !シグナルレイダーズ/Main.cpp
+++ !シグナルレイダーズ/Main.cpp
@@ -23,7 +23,7 @@ namespace
 	std::unique_ptr<Game> g_game;
 }
 
-LPCWSTR g_szAppName = L"3DPG応用";
+LPCWSTR g_szAppName = L"シグナルレイダーズ";
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void ExitGame() noexcept;
