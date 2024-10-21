fc1550315e9b7f3cbfd4c823c9710107df4de949
Kumachan1123 <kumakei2004@gmail.com>
2024-10-21 17:52:35 +0900

    フェード処理を改善

--- !シグナルレイダーズ/Resources/Shaders/Fade/PS_Fade.hlsl
+++ !シグナルレイダーズ/Resources/Shaders/Fade/PS_Fade.hlsl
@@ -1,11 +1,10 @@
 // C++側から設定されるデータ
 cbuffer ConstBuffer : register(b0)
 {
-    
     float power; // 強度
     float fadeAmount; // フェードの進行度（0.0～1.0）
     float padding; // パディング
-    int num; // テクスチャ番号 
+    int num; // テクスチャ番号
 };
 
 // テクスチャとサンプラー
@@ -25,34 +24,21 @@ struct PS_INPUT
 // ディゾルブエフェクト用ピクセルシェーダー
 float4 main(PS_INPUT input) : SV_TARGET
 {
-   
-      // フェードテクスチャの明度を取得
+    // フェードテクスチャの明度を取得
     float fadeValue = fadeTex.Sample(samLinear, input.Tex).r;
-    float4 mainValue = float4(1, 1, 1, 1); // 仮の値
-    // テクスチャ番号に応じて取得するテクスチャが変わる
-    switch (num)
-    {
-        case 0:
-           
-            mainValue = BlackTex.Sample(samLinear, input.Tex); //黒塗り
-            break;
-        case 1:
-            mainValue = readyTex.Sample(samLinear, input.Tex); //待機テクスチャ
-            break;
-        case 2:
-            mainValue = goTex.Sample(samLinear, input.Tex); //ゴー
-            break;
-    }
-    // 指定された番号に対応する画像がなかったら
-    if (mainValue.a == 0)
-    {
-        return float4(0, 0, 0, 0);
-    }
-    
-   
+
+    // 各テクスチャをサンプリング
+    float4 blackSample = BlackTex.Sample(samLinear, input.Tex);
+    float4 readySample = readyTex.Sample(samLinear, input.Tex);
+    float4 goSample = goTex.Sample(samLinear, input.Tex);
+
+    // num に基づいてテクスチャを選択（線形補間またはステップ関数を使用）
+    float4 mainValue = lerp(blackSample, readySample, step(1, num));
+    mainValue = lerp(mainValue, goSample, step(2, num));
+
     // フェード処理の強度を元にブレンドファクターを計算
     float alpha = smoothstep(fadeAmount - power, fadeAmount + power, fadeValue);
 
-    // 黒塗りのテクスチャとフェード処理を合成
-    return float4(mainValue.r, mainValue.g, mainValue.b, alpha);
+    // テクスチャとフェード処理を合成
+    return float4(mainValue.rgb, alpha);
 }
