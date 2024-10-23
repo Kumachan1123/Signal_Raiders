e2b6c90d94884a7ae21e90f1d37577a79c512b3e
Kumachan1123 <kumakei2004@gmail.com>
2024-10-23 20:31:32 +0900

    Releaseビルド

--- !シグナルレイダーズ/Main.cpp
+++ !シグナルレイダーズ/Main.cpp
@@ -56,6 +56,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	g_game = std::make_unique<Game>();
 	static bool s_fullscreen = true;;
 #ifdef _DEBUG
+
+
+#endif
+
 	// 画面モード選択
 	if (MessageBox(NULL, L"フルスクリーンにしますか？", L"画面モード設定", MB_YESNO) == IDYES)
 	{
@@ -65,10 +69,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	{
 		s_fullscreen = false;
 	}
-
-#endif
-
-
 	// Register class and create window
 	{
 		// Register class
@@ -168,194 +168,194 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	switch (message)
 	{
-		case WM_PAINT:
-			if (s_in_sizemove && game)
+	case WM_PAINT:
+		if (s_in_sizemove && game)
+		{
+			game->Tick();
+		}
+		else
+		{
+			PAINTSTRUCT ps;
+			std::ignore = BeginPaint(hWnd, &ps);
+			EndPaint(hWnd, &ps);
+		}
+		break;
+
+	case WM_DISPLAYCHANGE:
+		if (game)
+		{
+			game->OnDisplayChange();
+		}
+		break;
+
+	case WM_MOVE:
+		if (game)
+		{
+			game->OnWindowMoved();
+		}
+		break;
+
+		//case WM_SIZE:
+		//	if (wParam == SIZE_MINIMIZED)
+		//	{
+		//		if (!s_minimized)
+		//		{
+		//			s_minimized = true;
+		//			if (!s_in_suspend && game)
+		//				game->OnSuspending();
+		//			s_in_suspend = true;
+		//		}
+		//	}
+		//	else if (s_minimized)
+		//	{
+		//		s_minimized = false;
+		//		if (s_in_suspend && game)
+		//			game->OnResuming();
+		//		s_in_suspend = false;
+		//	}
+		//	// フルスクリーン切り替え時にコメント化すると、画面サイズがリサイズされずに最大化できる
+		//	// フルスクリーン時でも、見た目の1280x720サイズを保持する
+		//	//else if (!s_in_sizemove && game)
+		//	//{
+		//	//    game->OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
+		//	//}
+		//	break;
+
+	case WM_ENTERSIZEMOVE:
+		s_in_sizemove = true;
+		break;
+
+	case WM_EXITSIZEMOVE:
+		s_in_sizemove = false;
+		if (game)
+		{
+			RECT rc;
+			GetClientRect(hWnd, &rc);
+
+			game->OnWindowSizeChanged(rc.right - rc.left, rc.bottom - rc.top);
+		}
+		break;
+
+	case WM_GETMINMAXINFO:
+		if (lParam)
+		{
+			auto info = reinterpret_cast<MINMAXINFO*>(lParam);
+			info->ptMinTrackSize.x = 320;
+			info->ptMinTrackSize.y = 200;
+		}
+		break;
+
+	case WM_ACTIVATEAPP:
+		if (game)
+		{
+			if (wParam)
 			{
-				game->Tick();
+				game->OnActivated();
 			}
 			else
 			{
-				PAINTSTRUCT ps;
-				std::ignore = BeginPaint(hWnd, &ps);
-				EndPaint(hWnd, &ps);
+				game->OnDeactivated();
 			}
-			break;
+		}
 
-		case WM_DISPLAYCHANGE:
-			if (game)
+		// ★追記★
+		Keyboard::ProcessMessage(message, wParam, lParam);
+		Mouse::ProcessMessage(message, wParam, lParam);
+
+		break;
+
+	case WM_POWERBROADCAST:
+		switch (wParam)
+		{
+		case PBT_APMQUERYSUSPEND:
+			if (!s_in_suspend && game)
+				game->OnSuspending();
+			s_in_suspend = true;
+			return TRUE;
+
+		case PBT_APMRESUMESUSPEND:
+			if (!s_minimized)
 			{
-				game->OnDisplayChange();
+				if (s_in_suspend && game)
+					game->OnResuming();
+				s_in_suspend = false;
 			}
-			break;
+			return TRUE;
+		}
+		break;
 
-		case WM_MOVE:
-			if (game)
-			{
-				game->OnWindowMoved();
-			}
-			break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
 
-			//case WM_SIZE:
-			//	if (wParam == SIZE_MINIMIZED)
-			//	{
-			//		if (!s_minimized)
-			//		{
-			//			s_minimized = true;
-			//			if (!s_in_suspend && game)
-			//				game->OnSuspending();
-			//			s_in_suspend = true;
-			//		}
-			//	}
-			//	else if (s_minimized)
-			//	{
-			//		s_minimized = false;
-			//		if (s_in_suspend && game)
-			//			game->OnResuming();
-			//		s_in_suspend = false;
-			//	}
-			//	// フルスクリーン切り替え時にコメント化すると、画面サイズがリサイズされずに最大化できる
-			//	// フルスクリーン時でも、見た目の1280x720サイズを保持する
-			//	//else if (!s_in_sizemove && game)
-			//	//{
-			//	//    game->OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
-			//	//}
-			//	break;
+		// ★追記ココから★
+	case WM_ACTIVATE:
+	case WM_INPUT:
+	case WM_MOUSEMOVE:
+	case WM_LBUTTONDOWN:
+	case WM_LBUTTONUP:
+	case WM_RBUTTONDOWN:
+	case WM_RBUTTONUP:
+	case WM_MBUTTONDOWN:
+	case WM_MBUTTONUP:
+	case WM_MOUSEWHEEL:
+	case WM_XBUTTONDOWN:
+	case WM_XBUTTONUP:
+	case WM_MOUSEHOVER:
+		Mouse::ProcessMessage(message, wParam, lParam);
+		break;
 
-		case WM_ENTERSIZEMOVE:
-			s_in_sizemove = true;
-			break;
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+	case WM_SYSKEYUP:
+		Keyboard::ProcessMessage(message, wParam, lParam);
+		break;
+		// ★追記ココまで★
 
-		case WM_EXITSIZEMOVE:
-			s_in_sizemove = false;
-			if (game)
-			{
-				RECT rc;
-				GetClientRect(hWnd, &rc);
+	//case WM_SYSKEYDOWN:
+	//	if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
+	//	{
+	//		// Implements the classic ALT+ENTER fullscreen toggle
+	//		if (s_fullscreen)
+	//		{
+	//			SetWindowLongPtr(hWnd, GWL_STYLE, WS_MY_WINDOW);    // ★変更::WS_OVERLAPPEDWINDOW->WS_MY_WINDOW
+	//			SetWindowLongPtr(hWnd, GWL_EXSTYLE, 0);
 
-				game->OnWindowSizeChanged(rc.right - rc.left, rc.bottom - rc.top);
-			}
-			break;
+	//			int width = 800;
+	//			int height = 600;
+	//			if (game)
+	//				game->GetDefaultSize(width, height);
 
-		case WM_GETMINMAXINFO:
-			if (lParam)
-			{
-				auto info = reinterpret_cast<MINMAXINFO*>(lParam);
-				info->ptMinTrackSize.x = 320;
-				info->ptMinTrackSize.y = 200;
-			}
-			break;
+	//			ShowWindow(hWnd, SW_SHOWNORMAL);
 
-		case WM_ACTIVATEAPP:
-			if (game)
-			{
-				if (wParam)
-				{
-					game->OnActivated();
-				}
-				else
-				{
-					game->OnDeactivated();
-				}
-			}
+	//			// ウィンドウを元のサイズに戻すときにサイズを補正する
+	//			RECT rc{};
+	//			AdjustWindowRect(&rc, WS_MY_WINDOW, FALSE);
+	//			SetWindowPos(
+	//				hWnd, HWND_TOP,
+	//				0, 0,
+	//				width + rc.right - rc.left, height + rc.bottom - rc.top,
+	//				SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED
+	//			);
+	//		}
+	//		else
+	//		{
+	//			SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
+	//			SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_TOPMOST);
 
-			// ★追記★
-			Keyboard::ProcessMessage(message, wParam, lParam);
-			Mouse::ProcessMessage(message, wParam, lParam);
+	//			SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
 
-			break;
+	//			ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+	//		}
 
-		case WM_POWERBROADCAST:
-			switch (wParam)
-			{
-				case PBT_APMQUERYSUSPEND:
-					if (!s_in_suspend && game)
-						game->OnSuspending();
-					s_in_suspend = true;
-					return TRUE;
+	//		s_fullscreen = !s_fullscreen;
+	//	}
+	//	break;
 
-				case PBT_APMRESUMESUSPEND:
-					if (!s_minimized)
-					{
-						if (s_in_suspend && game)
-							game->OnResuming();
-						s_in_suspend = false;
-					}
-					return TRUE;
-			}
-			break;
-
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-
-			// ★追記ココから★
-		case WM_ACTIVATE:
-		case WM_INPUT:
-		case WM_MOUSEMOVE:
-		case WM_LBUTTONDOWN:
-		case WM_LBUTTONUP:
-		case WM_RBUTTONDOWN:
-		case WM_RBUTTONUP:
-		case WM_MBUTTONDOWN:
-		case WM_MBUTTONUP:
-		case WM_MOUSEWHEEL:
-		case WM_XBUTTONDOWN:
-		case WM_XBUTTONUP:
-		case WM_MOUSEHOVER:
-			Mouse::ProcessMessage(message, wParam, lParam);
-			break;
-
-		case WM_KEYDOWN:
-		case WM_KEYUP:
-		case WM_SYSKEYUP:
-			Keyboard::ProcessMessage(message, wParam, lParam);
-			break;
-			// ★追記ココまで★
-
-		//case WM_SYSKEYDOWN:
-		//	if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
-		//	{
-		//		// Implements the classic ALT+ENTER fullscreen toggle
-		//		if (s_fullscreen)
-		//		{
-		//			SetWindowLongPtr(hWnd, GWL_STYLE, WS_MY_WINDOW);    // ★変更::WS_OVERLAPPEDWINDOW->WS_MY_WINDOW
-		//			SetWindowLongPtr(hWnd, GWL_EXSTYLE, 0);
-
-		//			int width = 800;
-		//			int height = 600;
-		//			if (game)
-		//				game->GetDefaultSize(width, height);
-
-		//			ShowWindow(hWnd, SW_SHOWNORMAL);
-
-		//			// ウィンドウを元のサイズに戻すときにサイズを補正する
-		//			RECT rc{};
-		//			AdjustWindowRect(&rc, WS_MY_WINDOW, FALSE);
-		//			SetWindowPos(
-		//				hWnd, HWND_TOP,
-		//				0, 0,
-		//				width + rc.right - rc.left, height + rc.bottom - rc.top,
-		//				SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED
-		//			);
-		//		}
-		//		else
-		//		{
-		//			SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
-		//			SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_TOPMOST);
-
-		//			SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
-
-		//			ShowWindow(hWnd, SW_SHOWMAXIMIZED);
-		//		}
-
-		//		s_fullscreen = !s_fullscreen;
-		//	}
-		//	break;
-
-		case WM_MENUCHAR:
-			// A menu is active and the user presses a key that does not correspond
-			// to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
-			return MAKELRESULT(0, MNC_CLOSE);
+	case WM_MENUCHAR:
+		// A menu is active and the user presses a key that does not correspond
+		// to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
+		return MAKELRESULT(0, MNC_CLOSE);
 	}
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
