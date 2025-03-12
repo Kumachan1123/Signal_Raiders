4ed6a48ab1850e2a8562d5ea46f2ababd320223e
Kumachan1123 <kumakei2004@gmail.com>
2025-03-12 14:17:10 +0900

    EnemyAttackのコメント書き直し

--- Signal_Raiders/Game/Enemy/EnemyAI/EnemyAttack/EnemyAttack.cpp
+++ Signal_Raiders/Game/Enemy/EnemyAI/EnemyAttack/EnemyAttack.cpp
@@ -3,48 +3,51 @@
 	@brief	敵攻撃クラス
 */
 #include "pch.h"
-#include <SimpleMath.h>
-#include "Game/Enemy/EnemyAI/EnemyAttack/EnemyAttack.h"
-#include "Game/CommonResources.h"
-#include "DeviceResources.h"
-#include "Game/Enemy/EnemyAI/EnemyAI.h"
-#include "Libraries/MyLib/DebugString.h"
-#include "Libraries/MyLib/InputManager.h"
-#include "Libraries/MyLib/MemoryLeakDetector.h"
-#include <cassert>
-#include <random>  
-#include "Game/KumachiLib/KumachiLib.h"
-#include "Game/Enemy/Parameters/EnemyParameters.h"
+#include "EnemyAttack.h"
+
 using namespace DirectX::SimpleMath;
-// コンストラクタ
-EnemyAttack::EnemyAttack(EnemyAI* enemy)
-	: m_enemy(enemy)
-	, m_attackCooldown{ EnemyParameters::ATTACK_COOLDOWN }
+
+/*
+*	@brief	コンストラクタ
+*	@param	EnemyAI* enemy
+*	@return	なし
+*/
+EnemyAttack::EnemyAttack(EnemyAI* enemyAI)
+	: m_enemyAI(enemyAI)// 敵AI
+	, m_attackCooldown(EnemyParameters::ATTACK_COOLDOWN)// 攻撃のクールダウンタイム
 	, m_rotationSpeed{}
 	, m_commonResources{}
 {
 }
-// デストラクタ
+/*
+*	@brief	デストラクタ
+*	@return	なし
+*/
 EnemyAttack::~EnemyAttack() {}
-// 初期化する
+/*
+*	@brief	初期化
+*	@return	なし
+*/
 void EnemyAttack::Initialize()
 {
-	m_rotation = m_enemy->GetRotation();
-	m_velocity = m_enemy->GetVelocity();
-	m_scale = m_enemy->GetScale();
+	m_rotation = m_enemyAI->GetRotation();// 回転
+	m_velocity = m_enemyAI->GetVelocity();// 移動速度
+	m_scale = m_enemyAI->GetScale();// サイズ
 	m_rotationSpeed = EnemyParameters::INITIAL_ROTATION_SPEED; // 回転速度
 }
-
+/*
+*	@brief	更新
+*	@param	float elapsedTime
+*	@return	なし
+*/
 void EnemyAttack::Update(float elapsedTime)
 {
-	using namespace DirectX::SimpleMath;
 	m_rotationSpeed -= (elapsedTime / EnemyParameters::ROTATION_SPEED_DIVISOR);// 回転速度を減少
 	if (m_rotationSpeed <= EnemyParameters::ROTATION_SPEED_MIN)// 回転速度が基準以下になったら
 	{
 		m_rotationSpeed = EnemyParameters::ROTATION_SPEED_MIN;// 回転速度を設定
 	}
-	// プレイヤーへのベクトルを計算
-	Vector3 toPlayerVector = m_enemy->GetEnemy()->GetPlayer()->GetPlayerPos() - m_enemy->GetPosition();
+	Vector3 toPlayerVector = m_enemyAI->GetEnemy()->GetPlayer()->GetPlayerPos() - m_enemyAI->GetPosition();	// プレイヤーへのベクトルを計算
 	if (toPlayerVector.LengthSquared() > 0.0f)// プレイヤーへのベクトルが0より大きい場合
 	{
 		toPlayerVector.Normalize();// プレイヤーへのベクトルを正規化
@@ -55,36 +58,24 @@ void EnemyAttack::Update(float elapsedTime)
 	{
 		forward.Normalize();// 現在の前方ベクトルを正規化
 	}
-	// 内積を使って角度を計算
-	float dot = toPlayerVector.Dot(forward);
+	float dot = toPlayerVector.Dot(forward);	// 内積を使って角度を計算
 	dot = Clamp(dot, EnemyParameters::DOT_CLAMP.min, EnemyParameters::DOT_CLAMP.max); // acosの引数が範囲外になるのを防ぐため
-	float angle = std::acos(dot);
-	// 外積を使って回転方向を決定
-	Vector3 cross = toPlayerVector.Cross(forward);
-	if (cross.y < 0)angle = -angle;// プレイヤーの方向に向くための回転を計算
-	// プレイヤーの方向に向くための回転を計算
-	Quaternion toPlayerRotation = Quaternion::CreateFromAxisAngle(Vector3::Up, angle);
-
-	// 目標角度と現在の角度が非常に小さい場合、振動を防ぐために補間を止める
-	if (std::abs(angle) > EnemyParameters::ANGLE_THRESHOLD)
+	float angle = std::acos(dot);// プレイヤーの方向に向くための回転を計算
+	Vector3 cross = toPlayerVector.Cross(forward);// 外積を使って回転方向を計算
+	if (cross.y < 0)angle = -angle;// yが0より小さい場合、角度を反転
+	Quaternion toPlayerRotation = Quaternion::CreateFromAxisAngle(Vector3::Up, angle);// プレイヤーの方向に向くための回転を計算
+	if (std::abs(angle) > EnemyParameters::ANGLE_THRESHOLD)	// 目標角度と現在の角度が非常に小さい場合、振動を防ぐために補間を止める
 	{
-		m_rotation = Quaternion::Slerp(m_rotation, toPlayerRotation * m_rotation, elapsedTime * (m_rotationSpeed));
-		m_rotation.Normalize();
+		m_rotation = Quaternion::Slerp(m_rotation, toPlayerRotation * m_rotation, elapsedTime * (m_rotationSpeed));// 回転を設定
+		m_rotation.Normalize();// 回転を正規化
 	}
-
-	// プレイヤーの方向に移動
-	m_enemy->SetPosition(m_enemy->GetPosition() + toPlayerVector * (m_velocity.Length() * EnemyParameters::VELOCITY_SCALE_FACTOR) * elapsedTime);
-
-	// 攻撃のクールダウンタイムを管理
-	m_attackCooldown -= elapsedTime;
+	m_enemyAI->SetPosition(m_enemyAI->GetPosition() + toPlayerVector * (m_velocity.Length() * EnemyParameters::VELOCITY_SCALE_FACTOR) * elapsedTime);// プレイヤーの方向に移動
+	m_attackCooldown -= elapsedTime;// 攻撃のクールダウンタイムを減少
 	if (m_attackCooldown <= EnemyParameters::CHANGE_FACE_TIME)// 攻撃のクールダウンタイムが2.0以下になったら
 	{
-		m_enemy->SetState(IState::EnemyState::ANGRY);// 怒り態勢にする
-		// 次の攻撃までのクールダウンタイムを3秒に設定
-		if (m_attackCooldown <= 0.0f)m_attackCooldown = EnemyParameters::ATTACK_COOLDOWN;// 弾を発射
-
+		m_enemyAI->SetState(IState::EnemyState::ANGRY);// 怒り態勢にする
+		if (m_attackCooldown <= 0.0f)m_attackCooldown = EnemyParameters::ATTACK_COOLDOWN;// 次の攻撃までのクールダウンタイムを3秒に設定
 	}
-	// 敵の情報を更新
-	m_enemy->SetRotation(m_rotation);
-	m_enemy->SetVelocity(m_velocity);
+	m_enemyAI->SetRotation(m_rotation);// 回転を設定
+	m_enemyAI->SetVelocity(m_velocity);// 移動速度を設定
 }
--- Signal_Raiders/Game/Enemy/EnemyAI/EnemyAttack/EnemyAttack.h
+++ Signal_Raiders/Game/Enemy/EnemyAI/EnemyAttack/EnemyAttack.h
@@ -1,48 +1,67 @@
 /*
-	@file	Attack.h
+	@file	EnemyAttack.h
 	@brief	敵Attackクラス
 */
 
 #pragma once
 #ifndef ENEMY_ATTACK_DEFINED
 #define ENEMY_ATTACK_DEFINED
+// 標準ライブラリ
+#include <SimpleMath.h>
+#include <cassert>
+#include <random>  
+// 外部ライブラリ
+#include "Game/CommonResources.h"
+#include "DeviceResources.h"
+#include "Libraries/MyLib/DebugString.h"
+#include "Libraries/MyLib/InputManager.h"
+#include "Libraries/MyLib/MemoryLeakDetector.h"
+// 自作ヘッダーファイル
 #include "Game/Enemy/EnemyBullet/EnemyBullet.h"
 #include "Game/Interface/IState.h"
-class CommonResources;
+#include "Game/KumachiLib/KumachiLib.h"
+#include "Game/Enemy/EnemyAI/EnemyAI.h"
+#include "Game/Enemy/Parameters/EnemyParameters.h"
+
 //前方宣言
+class CommonResources;
 class EnemyAI;
 
 
 class EnemyAttack : public IState
 {
+public:
+	// アクセサ
+	DirectX::SimpleMath::Vector3 GetPosition() const { return m_position; }// 位置を取得
+	void SetPosition(DirectX::SimpleMath::Vector3 pos) { m_position = pos; }// 位置を設定
+	DirectX::SimpleMath::Quaternion GetRotation() const { return m_rotation; }// 回転を取得
+	void SetRotation(DirectX::SimpleMath::Quaternion rot) { m_rotation = rot; }// 回転を設定
+	float GetCoolTime() const { return m_attackCooldown; }// クールタイムを取得
+	void SetCoolTime(float coolTime) { m_attackCooldown = coolTime; }// クールタイムを設定
+	void SetScale(DirectX::SimpleMath::Vector3 sca) { m_scale = sca; }// サイズを設定
+	void SetRotationSpeed(float speed) { m_rotationSpeed = speed; }// 回転速度を設定
+public:
+	// publicメンバ関数
+	EnemyAttack(EnemyAI* enemyAI);// コンストラクタ
+	~EnemyAttack();	// デストラクタ
+	void Initialize() override;// 初期化する
+	void Update(float elapsedTime) override;// 更新する
 private:
 	// 共通リソース
 	CommonResources* m_commonResources;
-	EnemyAI* m_enemy;//敵AI
-	DirectX::SimpleMath::Vector3 m_position;//移動
-	DirectX::SimpleMath::Quaternion m_rotation;//回転
-	DirectX::SimpleMath::Vector3 m_velocity;// 移動速度
-	DirectX::SimpleMath::Vector3 m_scale;//サイズ
-
-	float m_rotationSpeed;//回転速度
-	float m_attackCooldown;  // 攻撃のクールダウンタイム
-
-public:
-	//	getter
-	DirectX::SimpleMath::Vector3 GetPosition() const { return m_position; }
-	DirectX::SimpleMath::Quaternion GetRotation() const { return m_rotation; }
-	float GetCoolTime() { return m_attackCooldown; }
-	//  setter
-	void SetPosition(DirectX::SimpleMath::Vector3 pos) { m_position = pos; }
-	void SetRotation(DirectX::SimpleMath::Quaternion rot) { m_rotation = rot; }
-	void SetScale(DirectX::SimpleMath::Vector3 sca) { m_scale = sca; }
-	void SetCoolTime(float coolTime) { m_attackCooldown = coolTime; }
-	void SetRotationSpeed(float speed) { m_rotationSpeed = speed; }
-public:
-	EnemyAttack(EnemyAI* enemy);
-	~EnemyAttack();
-
-	void Initialize() override;
-	void Update(float elapsedTime) override;
+	// 敵AI
+	EnemyAI* m_enemyAI;
+	// 移動
+	DirectX::SimpleMath::Vector3 m_position;
+	// 回転
+	DirectX::SimpleMath::Quaternion m_rotation;
+	// 移動速度
+	DirectX::SimpleMath::Vector3 m_velocity;
+	// サイズ
+	DirectX::SimpleMath::Vector3 m_scale;
+	// 回転速度
+	float m_rotationSpeed;
+	// 攻撃のクールダウンタイム
+	float m_attackCooldown;
 };
 #endif //ENEMY_ATTACK_DEFINED
