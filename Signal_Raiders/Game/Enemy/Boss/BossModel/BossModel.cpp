54e4656be31910b37092d893e80a3e0f512b553f
Kumachan1123 <kumakei2004@gmail.com>
2025-03-12 13:35:33 +0900

    BossModelのコメント書き直し

--- Signal_Raiders/Game/Enemy/Boss/BossModel/BossModel.cpp
+++ Signal_Raiders/Game/Enemy/Boss/BossModel/BossModel.cpp
@@ -3,80 +3,85 @@
 	@brief	敵モデルクラス
 */
 #include "pch.h"
-#include "Game/Enemy/Boss/Boss.h"
 #include "BossModel.h"
-#include "Game/CommonResources.h"
-#include "DeviceResources.h"
-#include <SimpleMath.h>
-#include "Libraries/Microsoft/ReadData.h"
-#include "Game/KumachiLib/KumachiLib.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+/*
+*	@breif	コンストラクタ
+*	@return	なし
+*/
 BossModel::BossModel()
-	: m_commonResources{}
-	, m_bodyModel{}
-	, m_pixelShader{}
-	, m_idlingFaceModel{}
-	, m_attackFaceModel{}
-	, m_isSheild{ false }
-	, m_sheildSize{ 0,0,0 }
-	, m_nowState{ IState::EnemyState::IDLING }
+	: m_commonResources{}// 共通リソース
+	, m_bodyModel{}// 胴体
+	, m_pixelShader{}// 影用のピクセルシェーダー
+	, m_idlingFaceModel{}// 普段の顔
+	, m_attackFaceModel{}// 攻撃時の顔
+	, m_isSheild(false)// シールド展開フラグ
+	, m_sheildSize(DirectX::SimpleMath::Vector3::Zero)// シールドのサイズ
+	, m_nowState(IState::EnemyState::IDLING)// 現在のステート
 {}
 
+/*
+*	@breif	デストラクタ
+*	@return	なし
+*/
 BossModel::~BossModel() {}
 
+/*
+*	@breif	初期化
+*	@param[in]	CommonResources* resources
+*	@return	なし
+*/
 void BossModel::Initialize(CommonResources* resources)
 {
-	using namespace DirectX;
-	using namespace DirectX::SimpleMath;
-	m_commonResources = resources;
-	auto device = resources->GetDeviceResources()->GetD3DDevice();
-
-	// 影用のシェーダーを読み込む
-	std::vector<uint8_t> ps = DX::ReadData(L"Resources/Shaders/Shadow/PS_Shadow.cso");
-	DX::ThrowIfFailed(device->CreatePixelShader(ps.data(), ps.size(), nullptr, m_pixelShader.ReleaseAndGetAddressOf()));
-	std::unique_ptr<DirectX::EffectFactory> fx = std::make_unique<DirectX::EffectFactory>(device);
-	fx->SetDirectory(L"Resources/Models/Boss");
-	// モデルを読み込む（ 胴体、表情差分）
-	m_bodyModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss.cmo", *fx);
-	m_idlingFaceModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Face_Idling.cmo", *fx);
-	m_attackFaceModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Face_Attack.cmo", *fx);
+	m_commonResources = resources;// 共通リソース
+	auto device = resources->GetDeviceResources()->GetD3DDevice();// デバイス
+	std::vector<uint8_t> ps = DX::ReadData(L"Resources/Shaders/Shadow/PS_Shadow.cso");// ピクセルシェーダーの読み込み
+	DX::ThrowIfFailed(device->CreatePixelShader(ps.data(), ps.size(), nullptr, m_pixelShader.ReleaseAndGetAddressOf()));// ピクセルシェーダーの作成
+	std::unique_ptr<DirectX::EffectFactory> fx = std::make_unique<DirectX::EffectFactory>(device);// エフェクトファクトリー
+	fx->SetDirectory(L"Resources/Models/Boss");// モデルのディレクトリ
+	m_bodyModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss.cmo", *fx);// 胴体
+	m_idlingFaceModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Face_Idling.cmo", *fx);// 普段の顔
+	m_attackFaceModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Face_Attack.cmo", *fx);// 攻撃時の顔
 }
-
+/*
+*	@brief	描画
+*	@param[in]	context  デバイスコンテキスト
+*	@param[in]	states	コモンステート
+* 　@param[in]	world	ワールド行列
+*	@param[in]	view	ビュー行列
+*	@param[in]	proj	プロジェクション行列
+*	@return	なし
+*/
 void BossModel::Render(ID3D11DeviceContext1* context,
 	DirectX::DX11::CommonStates* states,
 	DirectX::SimpleMath::Matrix world,
 	DirectX::SimpleMath::Matrix view,
 	DirectX::SimpleMath::Matrix proj)
 {
-	using namespace DirectX;
-	using namespace DirectX::SimpleMath;
-
-	// ライトの方向
-	Vector3 lightDir = Vector3::UnitY;
-	lightDir.Normalize();
-	// 影行列の元を作る
-	Matrix shadowMatrix = Matrix::CreateShadow(Vector3::UnitY, Plane(0.0f, 1.0f, 0.0f, 0.01f));
-	shadowMatrix = world * shadowMatrix;
-	// 影描画
-	m_bodyModel->Draw(context, *states, shadowMatrix * Matrix::Identity, view, proj, true, [&]()
+	Vector3 lightDir = Vector3::UnitY;// ライトの方向
+	lightDir.Normalize();// 正規化
+	Matrix shadowMatrix = Matrix::CreateShadow(Vector3::UnitY, Plane(0.0f, 1.0f, 0.0f, 0.01f));	// 影行列の元を作る
+	shadowMatrix = world * shadowMatrix;// 影行列
+	m_bodyModel->Draw(context, *states, shadowMatrix * Matrix::Identity, view, proj, true, [&]()	// 影描画
 		{
-			context->OMSetBlendState(states->Opaque(), nullptr, 0xffffffff);
-			context->OMSetDepthStencilState(states->DepthNone(), 0);
-			context->RSSetState(states->CullClockwise());
-			context->PSSetShader(m_pixelShader.Get(), nullptr, 0);
+			context->OMSetBlendState(states->Opaque(), nullptr, 0xffffffff);// ブレンドステート
+			context->OMSetDepthStencilState(states->DepthNone(), 0);// 深度ステンシルステート
+			context->RSSetState(states->CullClockwise());// ラスタライザーステート
+			context->PSSetShader(m_pixelShader.Get(), nullptr, 0);// ピクセルシェーダー
 		});
 	m_bodyModel->Draw(context, *states, world, view, proj);// 胴体
-
-	// 表情差分
-	switch (m_nowState)
+	switch (m_nowState)// 現在のステートによって描画を変える
 	{
 	case IState::EnemyState::DAMAGE:// ダメージ
-	case IState::EnemyState::IDLING:// アイドリング
-		m_idlingFaceModel->Draw(context, *states, world, view, proj);
+	case IState::EnemyState::IDLING:// 待機
+		m_idlingFaceModel->Draw(context, *states, world, view, proj);// 普段の顔
 		break;
 	case IState::EnemyState::ATTACK:// 攻撃
 	case IState::EnemyState::ANGRY:// 怒り
-		m_attackFaceModel->Draw(context, *states, world, view, proj);
+		m_attackFaceModel->Draw(context, *states, world, view, proj);// 攻撃時の顔
 		break;
-
 	}
 }
--- Signal_Raiders/Game/Enemy/Boss/BossModel/BossModel.h
+++ Signal_Raiders/Game/Enemy/Boss/BossModel/BossModel.h
@@ -5,42 +5,59 @@
 #pragma once
 #ifndef BOSS_MODEL_DEFINED
 #define BOSS_MODEL_DEFINED
+// 標準ライブラリ
+#include <SimpleMath.h>
+// 外部ライブラリ
 #include "Game/CommonResources.h"
+#include "DeviceResources.h"
+#include "Libraries/Microsoft/ReadData.h"
+// 自作ヘッダーファイル
+#include "Game/Enemy/Boss/Boss.h"
+#include "Game/KumachiLib/KumachiLib.h"
 #include "Game/Interface/IState.h"
+
+// クラスの前方宣言
 class Boss;
 class CommonResources;
 class IState;
+
+
 class BossModel
 {
-private:
-	// 共通リソース
-	CommonResources* m_commonResources;
-	// モデル
-	std::unique_ptr<DirectX::Model> m_bodyModel;//胴体
-
-	std::unique_ptr<DirectX::Model> m_attackFaceModel;//攻撃時の顔
-	std::unique_ptr<DirectX::Model> m_idlingFaceModel;//普段の顔
-
-	std::unique_ptr < DirectX::Model> m_sheildModel;//シールド
-	// モデルの影用のピクセルシェーダー
-	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
-
-	DirectX::SimpleMath::Vector3 m_sheildSize;
-	DirectX::SimpleMath::Vector3 m_sheildPosition;
-	DirectX::SimpleMath::Quaternion m_sheildRotation;
-	IState::EnemyState m_nowState;
-	// シールド展開フラグ
-	bool m_isSheild;
 public:
-	// 初期ステータスを設定
-	BossModel();
-	~BossModel();
-	void Initialize(CommonResources* resources);
-	void SetState(IState::EnemyState State) { m_nowState = State; }
-	void Render(ID3D11DeviceContext1* context,
+	// publicメンバ関数
+	BossModel();// コンストラクタ
+	~BossModel();// デストラクタ
+	void Initialize(CommonResources* resources);// 初期化
+	void SetState(IState::EnemyState State) { m_nowState = State; }// ステートの設定
+	void Render(ID3D11DeviceContext1* context,// 描画
 		DirectX::DX11::CommonStates* states,
 		DirectX::SimpleMath::Matrix world,
 		DirectX::SimpleMath::Matrix view,
 		DirectX::SimpleMath::Matrix proj);
+
+private:
+	// 共通リソース
+	CommonResources* m_commonResources;
+	// 胴体
+	std::unique_ptr<DirectX::Model> m_bodyModel;
+	// 攻撃時の顔
+	std::unique_ptr<DirectX::Model> m_attackFaceModel;
+	// 普段の顔
+	std::unique_ptr<DirectX::Model> m_idlingFaceModel;
+	// シールド
+	std::unique_ptr < DirectX::Model> m_sheildModel;
+	// モデルの影用のピクセルシェーダー
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
+	// シールドのサイズ
+	DirectX::SimpleMath::Vector3 m_sheildSize;
+	// シールドの位置
+	DirectX::SimpleMath::Vector3 m_sheildPosition;
+	// シールドの回転
+	DirectX::SimpleMath::Quaternion m_sheildRotation;
+	// 現在のステート
+	IState::EnemyState m_nowState;
+	// シールド展開フラグ
+	bool m_isSheild;
 };
 #endif //BOSS_MODEL_DEFINED
