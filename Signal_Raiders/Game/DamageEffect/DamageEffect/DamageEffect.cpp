ad3d54c2eaca5fb8307442e01ef652ef879a385c
Kumachan1123 <kumakei2004@gmail.com>
2025-03-12 12:26:48 +0900

    DamageEffectのコメント書き直し

--- Signal_Raiders/Game/DamageEffect/DamageEffect/DamageEffect.cpp
+++ Signal_Raiders/Game/DamageEffect/DamageEffect/DamageEffect.cpp
@@ -1,113 +1,118 @@
-//--------------------------------------------------------------------------------------
-// File: DamageEffect.h
-//
-// 攻撃を受けた時の演出クラス
-//
-//-------------------------------------------------------------------------------------
+/*
+*	@file DamageEffect.cpp
+*	@brief 攻撃を受けた時の演出クラス
+*/
 #include "pch.h"
 #include "DamageEffect.h"
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
-/// <summary>
-/// インプットレイアウト
-///	</summary>
+/*
+*	@brief 頂点情報
+*/
 const std::vector<D3D11_INPUT_ELEMENT_DESC>  DamageEffect::INPUT_LAYOUT =
 {
 	{ "POSITION",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	{ "TEXCOORD",	0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(SimpleMath::Vector3), D3D11_INPUT_PER_VERTEX_DATA, 0 },
 };
 
-// コンストラクタ
+/*
+*	@brief コンストラクタ
+*	@param[in] resources CommonResourcesクラスのポインタ
+*	@return なし
+*/
 DamageEffect::DamageEffect(CommonResources* resources)
-	: m_commonResources{ resources }
-	, m_time{ 0.0f }
+	: m_commonResources(resources)
+	, m_time(0.0f)
 	, m_constBuffer{}
-	, m_pDR{ resources->GetDeviceResources() }
-	, m_pPlayer{ nullptr }
-	, m_enemyDirection{  }
-	, m_playEffect{ false }
-	, m_pDrawPolygon{ DrawPolygon::GetInstance() }
-	, m_pCreateShader{ CreateShader::GetInstance() }
+	, m_pDR(resources->GetDeviceResources())
+	, m_pPlayer(nullptr)
+	, m_enemyDirection{}
+	, m_playEffect(false)
+	, m_pDrawPolygon(DrawPolygon::GetInstance())
+	, m_pCreateShader(CreateShader::GetInstance())
 {
-	// 色の初期化
-	m_constBuffer.colors = DirectX::SimpleMath::Vector4(1.0f, 1.0f, 1.0f, 0.0f);
-	// シェーダー作成クラスの初期化
-	m_pCreateShader->Initialize(m_pDR->GetD3DDevice(), &INPUT_LAYOUT[0], static_cast<UINT>(INPUT_LAYOUT.size()), m_pInputLayout);
+	m_constBuffer.colors = DirectX::SimpleMath::Vector4(1.0f, 1.0f, 1.0f, 0.0f);	// 色の初期化
+	m_pCreateShader->Initialize(m_pDR->GetD3DDevice(), &INPUT_LAYOUT[0], static_cast<UINT>(INPUT_LAYOUT.size()), m_pInputLayout);	// シェーダー作成クラスの初期化
 }
 
-// デストラクタ
-DamageEffect::~DamageEffect()
-{
-}
+/*
+*	@brief デストラクタ
+*	@return なし
+*/
+DamageEffect::~DamageEffect() {}
 
-// 初期化
+/*
+*	@brief 初期化
+*	@param[in] pPlayer プレイヤーのポインタ
+*	@return なし
+*/
 void DamageEffect::Initialize(Player* pPlayer)
 {
-	m_pPlayer = pPlayer;
-	m_playEffect = true;
-	// シェーダーの作成
-	CreateShader();
-	// 画像の読み込み（2枚ともデフォルトは読み込み失敗でnullptr)
-	LoadTexture(L"Resources/Textures/crisis.png");
-	// 板ポリゴン描画用
-	m_pDrawPolygon->InitializePositionTexture(m_pDR);
-	// 撃ってきた敵の向きを取得
-	m_enemyDirection = m_pPlayer->GetEnemyBulletDirection();
+	m_pPlayer = pPlayer;// プレイヤーのポインタを受け取る
+	m_playEffect = true;// エフェクト再生フラグをtrueにする
+	MakeShader();	// シェーダーの作成
+	LoadTexture(L"Resources/Textures/crisis.png");// テクスチャ読み込み
+	m_pDrawPolygon->InitializePositionTexture(m_pDR);// 頂点情報の初期化
+	m_enemyDirection = m_pPlayer->GetEnemyBulletDirection();// 攻撃してきた敵の向きを取得
 }
 
-// テクスチャリソース読み込み関数
+/*
+*	@brief テクスチャリソース読み込み関数
+*	@param[in] path テクスチャのパス
+*	@return なし
+*/
 void  DamageEffect::LoadTexture(const wchar_t* path)
 {
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;
-	DirectX::CreateWICTextureFromFile(m_pDR->GetD3DDevice(), path, nullptr, texture.ReleaseAndGetAddressOf());
-	m_texture.push_back(texture);
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;// テクスチャリソースビュー
+	DirectX::CreateWICTextureFromFile(m_pDR->GetD3DDevice(), path, nullptr, texture.ReleaseAndGetAddressOf());// テクスチャの読み込み
+	m_texture.push_back(texture);// テクスチャリソースビューを格納
 }
 
 
 
-// シェーダー作成部分
-void  DamageEffect::CreateShader()
+/*
+*	@brief シェーダー作成
+*	@return なし
+*/
+void  DamageEffect::MakeShader()
 {
-	// 頂点シェーダー作成
-	m_pCreateShader->CreateVertexShader(L"Resources/Shaders/DamageEffect/VS_Damage.cso", m_vertexShader);
-	// ピクセルシェーダー作成
-	m_pCreateShader->CreatePixelShader(L"Resources/Shaders/DamageEffect/PS_Damage.cso", m_pixelShader);
-	// インプットレイアウトを受け取る
-	m_pInputLayout = m_pCreateShader->GetInputLayout();
-	//	シェーダーにデータを渡すためのコンスタントバッファ生成
-	m_pCreateShader->CreateConstantBuffer(m_cBuffer, sizeof(ConstBuffer));
-	// シェーダーの構造体にセット
-	m_shaders.vs = m_vertexShader.Get();
-	m_shaders.ps = m_pixelShader.Get();
-	m_shaders.gs = nullptr;
+	m_pCreateShader->CreateVertexShader(L"Resources/Shaders/DamageEffect/VS_Damage.cso", m_vertexShader);// 頂点シェーダー作成
+	m_pCreateShader->CreatePixelShader(L"Resources/Shaders/DamageEffect/PS_Damage.cso", m_pixelShader);	// ピクセルシェーダー作成
+	m_pInputLayout = m_pCreateShader->GetInputLayout();// 入力レイアウト取得
+	m_pCreateShader->CreateConstantBuffer(m_cBuffer, sizeof(ConstBuffer));// コンスタントバッファ作成
+	m_shaders.vs = m_vertexShader.Get();// 頂点シェーダーをセット
+	m_shaders.ps = m_pixelShader.Get();// ピクセルシェーダーをセット
+	m_shaders.gs = nullptr;// ジオメトリシェーダーは使わないのでnullptr
 }
 
-
-
-// 更新
+/*
+*	@brief 更新
+*	@param[in] elapsedTime 経過時間
+*	@return なし
+*/
 void  DamageEffect::Update(float elapsedTime)
 {
-	if (!m_playEffect)return;
-	// 時間更新
-	m_time += elapsedTime;
-	if (m_time >= PLAY_TIME)
+	if (!m_playEffect)return;// エフェクト再生フラグがfalseなら更新しない
+	m_time += elapsedTime;// 時間を加算
+	if (m_time >= PLAY_TIME)// 再生時間を過ぎたら
 	{
-		m_time = 0.0f;
-		m_constBuffer.colors.w = 0.0f;
-		m_playEffect = false;
-		m_pPlayer->SetisPlayEffect(m_playEffect);
-		return;
+		m_time = 0.0f;// 時間を初期化
+		m_constBuffer.colors.w = 0.0f;// アルファ値を初期化
+		m_playEffect = false;// エフェクト再生フラグをfalseにする
+		m_pPlayer->SetisPlayEffect(m_playEffect);// プレイヤーにエフェクト再生フラグを送る
+		return;// 更新終了
 	}
-	m_constBuffer.time = Vector4(m_time);
-	// アルファ値(sin関数を使って二秒以内に0から1を往復する)
-	m_constBuffer.colors.w = 0.5f + 0.5f * sin(m_time * 2.0f);
-	// 角度を計算
-	float angle = CalculateAngle();
-	m_constBuffer.uv = GetUVFromAngle(angle);
+	m_constBuffer.time = Vector4(m_time);// 時間をコンスタントバッファに格納
+	m_constBuffer.colors.w = 0.5f + 0.5f * sin(m_time * 2.0f);// アルファ値を計算
+	float angle = CalculateAngle();// 角度計算
+	m_constBuffer.uv = GetUVFromAngle(angle);// 角度からUV座標を求める
 }
-// 角度計算
+/*
+*	@brief 角度計算
+*	@return 角度
+*/
 float DamageEffect::CalculateAngle() const
 {
 	Vector3 playerDir = m_pPlayer->GetPlayerDir();// プレイヤーの向き
@@ -115,27 +120,27 @@ float DamageEffect::CalculateAngle() const
 	angle = DirectX::XMConvertToDegrees(angle);// ラジアンから度に変換
 	return (angle < 0) ? angle + 360 : angle;// 0から360の間に収める
 }
-// 角度からUV座標を求める
+/*
+*	@brief 角度からUV座標を求める
+*	@param[in] angle 角度
+*	@return UV座標
+*/
 DirectX::SimpleMath::Vector4 DamageEffect::GetUVFromAngle(float angle) const
 {
-	// 角度（degree）をラジアンに変換
-	float rad = DirectX::XMConvertToRadians(angle);
-
-	// 半径は左右または上下の中心からの距離
-	float radius = UV_W - UV_C;
-
-	// 座標を計算
-	float u = UV_C - radius * -sin(rad);
-	float v = UV_C + radius * cos(rad);
-
-	return DirectX::SimpleMath::Vector4(u, v, 0, 0);
+	float rad = DirectX::XMConvertToRadians(angle);// 度からラジアンに変換
+	float radius = UV_W - UV_C;	// 半径は左右または上下の中心からの距離
+	float u = UV_C - radius * -sin(rad);// 座標を計算U
+	float v = UV_C + radius * cos(rad);// 座標を計算V
+	return DirectX::SimpleMath::Vector4(u, v, 0, 0);// UV座標を返す
 }
-// 描画
+/*
+*	@brief 描画
+*	@return なし
+*/
 void  DamageEffect::Render()
 {
 
-	// 頂点情報(板ポリゴンの４頂点の座標情報）
-	VertexPositionTexture vertex[4] =
+	VertexPositionTexture vertex[4] =	// 頂点情報(板ポリゴンの４頂点の座標情報）
 	{
 		// 頂点情報													UV情報
 		VertexPositionTexture(SimpleMath::Vector3(-SIZE_X * SCALE,  SIZE_Y * SCALE, 0.0f), SimpleMath::Vector2(0.0f, 0.0f)),
@@ -143,28 +148,19 @@ void  DamageEffect::Render()
 		VertexPositionTexture(SimpleMath::Vector3(SIZE_X * SCALE, -SIZE_Y * SCALE, 0.0f),  SimpleMath::Vector2(1.0f, 1.0f)),
 		VertexPositionTexture(SimpleMath::Vector3(-SIZE_X * SCALE, -SIZE_Y * SCALE, 0.0f), SimpleMath::Vector2(0.0f, 1.0f)),
 	};
-	// シェーダーに渡す追加のバッファを作成する(ConstBuffer)
-	m_constBuffer.matView = m_view.Transpose();
-	m_constBuffer.matProj = m_proj.Transpose();
-	m_constBuffer.matWorld = m_world.Transpose();
-	// 受け渡し用バッファの内容更新(ConstBufferからID3D11Bufferへの変換）
-	m_pDrawPolygon->UpdateSubResources(m_cBuffer.Get(), &m_constBuffer);
-	// シェーダーにバッファを渡す
-	ID3D11Buffer* cb[1] = { m_cBuffer.Get() };
-	// 頂点シェーダもピクセルシェーダも、同じ値を渡す
-	m_pDrawPolygon->SetShaderBuffer(0, 1, cb);
-	// 描画前設定
-	m_pDrawPolygon->DrawSetting(
-		DrawPolygon::SamplerStates::LINEAR_WRAP,
-		DrawPolygon::BlendStates::NONPREMULTIPLIED,
-		DrawPolygon::RasterizerStates::CULL_NONE,
-		DrawPolygon::DepthStencilStates::DEPTH_NONE);
-	// 描画準備
-	m_pDrawPolygon->DrawStart(m_pInputLayout.Get(), m_texture);
-	// シェーダをセットする
-	m_pDrawPolygon->SetShader(m_shaders, nullptr, 0);
-	// 板ポリゴンを描画
-	m_pDrawPolygon->DrawTexture(vertex);
-	//	シェーダの登録を解除しておく
-	m_pDrawPolygon->ReleaseShader();
+	m_constBuffer.matView = m_view.Transpose();// ビュー行列を転置
+	m_constBuffer.matProj = m_proj.Transpose();// プロジェクション行列を転置
+	m_constBuffer.matWorld = m_world.Transpose();// ワールド行列を転置
+	m_pDrawPolygon->UpdateSubResources(m_cBuffer.Get(), &m_constBuffer);// コンスタントバッファの更新
+	ID3D11Buffer* cb[1] = { m_cBuffer.Get() };// コンスタントバッファのセット
+	m_pDrawPolygon->SetShaderBuffer(0, 1, cb);	// 頂点シェーダもピクセルシェーダも、同じ値を渡す
+	m_pDrawPolygon->DrawSetting(// 	// 描画前設定
+		DrawPolygon::SamplerStates::LINEAR_WRAP,// サンプラーステート
+		DrawPolygon::BlendStates::NONPREMULTIPLIED,// ブレンドステート
+		DrawPolygon::RasterizerStates::CULL_NONE,// ラスタライザーステート
+		DrawPolygon::DepthStencilStates::DEPTH_NONE);// 深度ステンシルステート
+	m_pDrawPolygon->DrawStart(m_pInputLayout.Get(), m_texture);// 描画開始
+	m_pDrawPolygon->SetShader(m_shaders, nullptr, 0);// シェーダーの登録
+	m_pDrawPolygon->DrawTexture(vertex);// 板ポリゴンを描画
+	m_pDrawPolygon->ReleaseShader();// シェーダー解放
 }
--- Signal_Raiders/Game/DamageEffect/DamageEffect/DamageEffect.h
+++ Signal_Raiders/Game/DamageEffect/DamageEffect/DamageEffect.h
@@ -1,13 +1,9 @@
-//--------------------------------------------------------------------------------------
-// File: DamageEffect.h
-//
-// 攻撃を受けた時の演出クラス
-//
-//-------------------------------------------------------------------------------------
-
+/*
+*	@file DamageEffect.h
+*	@brief 攻撃を受けた時の演出クラス
+*/
 #pragma once
-#include "Game/CommonResources.h"
-#include <DeviceResources.h>
+// 標準ライブラリ
 #include <SimpleMath.h>
 #include <Effects.h>
 #include <PrimitiveBatch.h>
@@ -15,17 +11,24 @@
 #include <WICTextureLoader.h>
 #include <CommonStates.h>
 #include <vector>
+// 外部ライブラリ
+#include "Game/CommonResources.h"
+#include <DeviceResources.h>
+// 自作ヘッダーファイル
 #include "Game/Player/Player.h"
 #include "Game/KumachiLib/CreateShader/CreateShader.h"
 #include "Game/KumachiLib/BinaryFile/BinaryFile.h"
 #include "Game/KumachiLib/DrawPolygon/DrawPolygon.h"
+
+// クラスの前方宣言
 class Player;
 class CommonResources;
+
 class DamageEffect
 {
 public:
-	// データ受け渡し用コンスタントバッファ(送信側)
-	struct ConstBuffer
+	// 構造体
+	struct ConstBuffer// データ受け渡し用コンスタントバッファ(送信側)
 	{
 		DirectX::SimpleMath::Matrix matWorld;   // ワールド行列
 		DirectX::SimpleMath::Matrix matView;    // ビュー行列
@@ -35,11 +38,39 @@ public:
 		DirectX::SimpleMath::Vector4 uv;// uv座標
 		DirectX::SimpleMath::Vector4 alpha;// アルファ値
 	}m_constBuffer;
-private:
-	// 変数
-	DX::DeviceResources* m_pDR;
 
-	Microsoft::WRL::ComPtr<ID3D11Buffer>	m_cBuffer;
+public:
+
+	// public関数
+	DamageEffect(CommonResources* resources);// コンストラクタ
+	~DamageEffect();// デストラクタ
+
+	void Initialize(Player* pPlayer);// 初期化
+	void LoadTexture(const wchar_t* path);// テクスチャリソース読み込み関数
+	void Update(float elapsedTime);// 更新
+	void Render();// 描画
+
+	// アクセサ
+	bool GetPlayEffect()const { return m_playEffect; }// エフェクト再生フラグ取得
+	DirectX::SimpleMath::Vector3 GetEnemyDirection()const { return m_enemyDirection; }// 攻撃してきた敵の向き取得
+	bool Destroy()const// 破棄判定
+	{
+		if (m_time >= DESTROY_TIME)return true;// 破棄時間を過ぎたらtrue
+		else return false;// それ以外はfalse
+	}
+	// public定数
+	// 頂点情報
+	static const std::vector<D3D11_INPUT_ELEMENT_DESC> INPUT_LAYOUT;
+private:
+	void MakeShader();// シェーダー作成
+	float CalculateAngle()const;// 角度計算
+	DirectX::SimpleMath::Vector4 GetUVFromAngle(float angle)const;// 角度からUV座標を求める
+private:
+	// private変数
+	// デバイスリソース
+	DX::DeviceResources* m_pDR;
+	// コンスタントバッファ
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_cBuffer;
 	// 共通リソース
 	CommonResources* m_commonResources;
 	// プレイヤーのポインター
@@ -48,36 +79,32 @@ private:
 	DrawPolygon* m_pDrawPolygon;
 	// シェーダー作成クラス
 	CreateShader* m_pCreateShader;
-
 	// 入力レイアウト
 	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_pInputLayout;
-
 	// プリミティブバッチ
 	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionTexture>> m_batch;
 	// コモンステート
 	std::unique_ptr<DirectX::CommonStates> m_states;
 	// テクスチャハンドル
 	std::vector<Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>> m_texture;
-
 	// 頂点シェーダ
 	Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
 	// ピクセルシェーダ
 	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
 	// シェーダーの構造体
 	DrawPolygon::Shaders m_shaders;
-
-	// ワールドビュープロジェクション行列
+	// ワールド行列
 	DirectX::SimpleMath::Matrix m_world;
+	// ビュー行列
 	DirectX::SimpleMath::Matrix m_view;
+	// プロジェクション行列
 	DirectX::SimpleMath::Matrix m_proj;
-
 	// 時間
 	float m_time;
 	// 攻撃してきた敵のいる向き
 	DirectX::SimpleMath::Vector3 m_enemyDirection;
 	// プレイヤーの向き
 	DirectX::SimpleMath::Vector3 m_playerDirection;
-
 	// 固定値
 	// 板ポリゴンのXサイズ
 	const float SIZE_X = 0.0625;
@@ -97,31 +124,4 @@ private:
 	const float DESTROY_TIME = 2.0f;
 	// エフェクト再生フラグ
 	bool m_playEffect;
-public:
-	//	関数
-	static const std::vector<D3D11_INPUT_ELEMENT_DESC> INPUT_LAYOUT;
-
-	DamageEffect(CommonResources* resources);
-	~DamageEffect();
-
-	void LoadTexture(const wchar_t* path);
-
-
-	void Initialize(Player* pPlayer);
-	void Update(float elapsedTime);
-
-	void Render();
-
-	// getter
-	bool GetPlayEffect()const { return m_playEffect; }
-	DirectX::SimpleMath::Vector3 GetEnemyDirection()const { return m_enemyDirection; }
-	bool Destroy()const
-	{
-		if (m_time >= DESTROY_TIME)return true;
-		else return false;
-	}
-private:
-	void CreateShader();// シェーダー作成
-	float CalculateAngle()const;// 角度計算
-	DirectX::SimpleMath::Vector4 GetUVFromAngle(float angle)const;// 角度からUV座標を求める
 };
