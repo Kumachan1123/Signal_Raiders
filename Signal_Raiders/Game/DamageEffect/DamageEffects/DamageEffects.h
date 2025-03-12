ef1a73f04e056698d6a111b4e4a5a04561831cf1
Kumachan1123 <kumakei2004@gmail.com>
2025-03-12 12:32:53 +0900

    DamageEffectsのコメント書き直し

--- Signal_Raiders/Game/DamageEffect/DamageEffects/DamageEffects.h
+++ Signal_Raiders/Game/DamageEffect/DamageEffects/DamageEffects.h
@@ -1,28 +1,35 @@
-// 攻撃を受けた時の演出を管理するクラス
+/*
+*	@file DamageEffects.h
+*	@brief ダメージエフェクトの管理クラスのヘッダーファイル
+*/
 #pragma once
+// 標準ライブラリ
+#include <vector>
+// 外部ライブラリ
 #include "Game/CommonResources.h"
+// 自作ヘッダーファイル
 #include "Game/Player/Player.h"
 #include "Game/DamageEffect/DamageEffect/DamageEffect.h"
+
+// クラスの前方宣言
 class Player;
 class CommonResources;
 class DamageEffect;
+
+
 class DamageEffects
 {
+public:
+	// publicメンバ関数
+	DamageEffects(CommonResources* resources);// コンストラクタ
+	~DamageEffects();// デストラクタ
+
+	void Create();	// ダメージを受けた時の演出を生成
+	void Initialize(Player* pPlayer);// 初期化
+	void Update(float elapsedTime);// 更新
+	void Render();// 描画
 private:
 	CommonResources* m_commonResources;// 共通リソース
 	Player* m_pPlayer;// プレイヤー
-	// ダメージエフェクト
-	std::vector<std::unique_ptr<DamageEffect>> m_pDamageEffect;
-public:
-	DamageEffects(CommonResources* resources);
-	~DamageEffects();
-
-	// ダメージを受けた時の演出を生成
-	void Create();
-
-	void Initialize(Player* pPlayer);
-	void Update(float elapsedTime);
-
-	void Render();
-
+	std::vector<std::unique_ptr<DamageEffect>> m_pDamageEffect;	// ダメージエフェクト
 };
--- Signal_Raiders/Game/DamageEffect/DamageEffects/DamegeEffects.cpp
+++ Signal_Raiders/Game/DamageEffect/DamageEffects/DamegeEffects.cpp
@@ -1,58 +1,73 @@
+/*
+*	@file DamageEffects.cpp
+*	@brief ダメージエフェクトの管理クラスのソースファイル
+*/
 #include <pch.h>
 #include "DamageEffects.h"
 
-// コンストラクタ
+/*
+*	@brief コンストラクタ
+*	@param[in] resources 共通リソース
+*	@return なし
+*/
 DamageEffects::DamageEffects(CommonResources* resources)
-	: m_commonResources{ resources }
+	: m_commonResources(resources)
 	, m_pPlayer{}
 {}
 
-// デストラクタ
-DamageEffects::~DamageEffects()
-{
-}
+/*
+*	@brief デストラクタ
+*	@return なし
+*/
+DamageEffects::~DamageEffects() {}
 
-// 初期化
+/*
+*	@brief 初期化
+*	@param[in] pPlayer プレイヤーのポインタ
+*	@return なし
+*/
 void DamageEffects::Initialize(Player* pPlayer)
 {
-	m_pPlayer = pPlayer;
+	m_pPlayer = pPlayer;// プレイヤーのポインターを受け取る
 }
 
-
-// ダメージを受けた時の演出を生成
+/*
+*	@brief ダメージを受けた時の演出を生成
+*	@return なし
+*/
 void DamageEffects::Create()
 {
-	std::unique_ptr<DamageEffect> damageEffect = std::make_unique<DamageEffect>(m_commonResources);
-	damageEffect->Initialize(m_pPlayer);
-	m_pDamageEffect.push_back(std::move(damageEffect));
-
+	std::unique_ptr<DamageEffect> damageEffect = std::make_unique<DamageEffect>(m_commonResources);// ダメージエフェクトの生成
+	damageEffect->Initialize(m_pPlayer);// 初期化
+	m_pDamageEffect.push_back(std::move(damageEffect));// ダメージエフェクトをリストに追加
 }
 
-// 更新
+/*
+*	@brief 更新
+*	@param[in] elapsedTime 経過時間
+*	@return なし
+*/
 void DamageEffects::Update(float elapsedTime)
 {
 	std::vector<std::unique_ptr<DamageEffect>> newDamageEffect;// 新しいダメージエフェクト
 	for (auto& damageEffect : m_pDamageEffect)
 	{
-		damageEffect->Update(elapsedTime);
+		damageEffect->Update(elapsedTime);// ダメージエフェクトを更新
 		// 再生が終わったダメージエフェクトだったらそのエフェクトを完全に破棄
-		if (damageEffect->Destroy())
+		if (damageEffect->Destroy())// ダメージエフェクトの破棄フラグがtrueだったら
 		{
-			damageEffect.reset();
-			continue;
+			damageEffect.reset();// ダメージエフェクトを破棄
+			continue;// 次のループへ
 		}
-		// 再生が終了していないエフェクトは新しいリストに移動
-		newDamageEffect.push_back(std::move(damageEffect));
+		newDamageEffect.push_back(std::move(damageEffect));// 新しいリストにダメージエフェクトを追加
 	}
-	// ダメージエフェクトを新しいリストに置き換える
-	m_pDamageEffect = std::move(newDamageEffect);
+	m_pDamageEffect = std::move(newDamageEffect);// ダメージエフェクトを新しいリストに置き換える
 }
-
+/*
+*	@brief 描画
+*	@return なし
+*/
 void DamageEffects::Render()
 {
-	// ダメージエフェクトを更新する
-	for (auto& damageEffect : m_pDamageEffect)
-	{
-		if (damageEffect->GetPlayEffect())damageEffect->Render();
-	}
+	for (auto& damageEffect : m_pDamageEffect)if (damageEffect->GetPlayEffect())damageEffect->Render();	// ダメージエフェクトを更新する
 }
