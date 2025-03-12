51fe1ec588d5dda74e0e114e97d6e8d76c02a44f
Kumachan1123 <kumakei2004@gmail.com>
2025-03-12 13:42:24 +0900

    BossSheildのコメント書き直し

--- Signal_Raiders/Game/Enemy/Boss/BossSheild/BossSheild.cpp
+++ Signal_Raiders/Game/Enemy/Boss/BossSheild/BossSheild.cpp
@@ -4,43 +4,46 @@
 */
 #include "pch.h"
 #include "BossSheild.h"
-#include "Game/CommonResources.h"
-#include "DeviceResources.h"
-#include <SimpleMath.h>
-#include "Libraries/Microsoft/ReadData.h"
-#include "Game/KumachiLib/KumachiLib.h"
-#include "Game/Enemy/Parameters/EnemyParameters.h"
+
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
+/*
+*	@brief	コンストラクタ
+*	@param sheildHP シールドの耐久値
+*	@param pBoss ボスクラスのポインタ
+*	@return	なし
+*/
 BossSheild::BossSheild(int sheildHP, Boss* pBoss)
-	: m_commonResources{}
-	, m_isSheild{ false }
-	, m_isParticle{ false }
-	, m_sheildSize{ 0,0,0 }
-	, m_sheildPosition{ 0,0,0 }
-	, m_sheildHP{ sheildHP }// シールドの耐久値
-	, m_pBoss{ pBoss }
-
+	: m_commonResources{}// 共通リソース
+	, m_isSheild(false)// シールド展開フラグ
+	, m_isParticle(false)// パーティクル再生フラグ
+	, m_sheildSize(Vector3::Zero)// シールドのサイズ
+	, m_sheildPosition(Vector3::Zero)// シールドの座標
+	, m_sheildHP(sheildHP)// シールドのHP
+	, m_pBoss(pBoss)// ボスクラスのポインタ
 {
 }
+/*
+*	@brief	デストラクタ
+*	@return	なし
+*/
+BossSheild::~BossSheild() {}
 
-BossSheild::~BossSheild()
-{
-}
-
+/*
+*	@brief	初期化
+*	@param resources 共通リソース
+*	@return	なし
+*/
 void BossSheild::Initialize(CommonResources* resources)
 {
-	m_commonResources = resources;
-	auto device = resources->GetDeviceResources()->GetD3DDevice();
-	// パーティクル生成
+	m_commonResources = resources;// 共通リソースを設定
+	auto device = resources->GetDeviceResources()->GetD3DDevice();// デバイス取得
 	m_pParticle = std::make_unique<Particle>(ParticleUtility::Type::BARRIERBREAK, 0.0f);// 0.0fなのはすでに内部で設定されているから
-	m_pParticle->Initialize(m_commonResources);
-
-	std::unique_ptr<DirectX::EffectFactory> fx = std::make_unique<DirectX::EffectFactory>(device);
-	fx->SetDirectory(L"Resources/Models/Boss");
-	// シールドモデルを読み込む
-	m_sheildModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Barrier.cmo", *fx);
+	m_pParticle->Initialize(m_commonResources);// パーティクル初期化
+	std::unique_ptr<DirectX::EffectFactory> fx = std::make_unique<DirectX::EffectFactory>(device);// エフェクトファクトリー作成
+	fx->SetDirectory(L"Resources/Models/Boss");// ディレクトリ設定
+	m_sheildModel = DirectX::Model::CreateFromCMO(device, L"Resources/Models/Boss/Boss_Barrier.cmo", *fx);// モデル作成
 }
 
 void BossSheild::Update(float elapsedTime)
@@ -48,17 +51,12 @@ void BossSheild::Update(float elapsedTime)
 	if (m_isSheild)// シールドが展開されている間
 	{
 		m_pBoss->PlayBarrierSE();// シールドSE再生
-		// シールドが展開されている間はサイズを拡大
-		m_sheildSize = Vector3::SmoothStep(m_sheildSize, Vector3::One, EnemyParameters::BOSS_SHIELD_SCALE_SPEED);
-		// ボスの境界球を大きくする
-		m_pBoss->GetBoundingSphere().Radius = EnemyParameters::BOSS_SHIELD_RADIUS;
-		// 攻撃の間隔を速くする
-		m_pBoss->SetBulletCooldown(EnemyParameters::BOSS_SHIELD_ATTACK_COOLDOWN);
-		// 特殊攻撃の間隔を速くする
-		m_pBoss->SetInitSpecialAttacCooldown(EnemyParameters::SPECIAL_ATTACK_COOLDOWN / 2);
+		m_sheildSize = Vector3::SmoothStep(m_sheildSize, Vector3::One, EnemyParameters::BOSS_SHIELD_SCALE_SPEED);// シールドのサイズを拡大
+		m_pBoss->GetBoundingSphere().Radius = EnemyParameters::BOSS_SHIELD_RADIUS;// ボスの境界球をシールドの大きさに合わせる
+		m_pBoss->SetBulletCooldown(EnemyParameters::BOSS_SHIELD_ATTACK_COOLDOWN);// 攻撃の間隔を速くする
+		m_pBoss->SetInitSpecialAttacCooldown(EnemyParameters::SPECIAL_ATTACK_COOLDOWN / 2);// 特殊攻撃の間隔を速くする
 	}
-	// シールドが破壊されたらパーティクルを再生
-	if (m_sheildHP <= 0)
+	if (m_sheildHP <= 0)// シールドが破壊されたら
 	{
 		m_isParticle = true;// パーティクル再生
 		m_isSheild = false;// シールド破壊
@@ -67,27 +65,30 @@ void BossSheild::Update(float elapsedTime)
 		m_pParticle->Update(elapsedTime);// パーティクル更新
 	}
 }
-
+/*
+*	@brief	描画
+*	@param context デバイスコンテキスト
+*	@param states ステート
+*	@param world ワールド行列
+*	@param view ビュー行列
+*	@param proj プロジェクション行列
+*	@return	なし
+*/
 void BossSheild::Render(ID3D11DeviceContext1* context, DirectX::DX11::CommonStates* states, Matrix world, Matrix view, Matrix proj)
 {
-	if (m_isSheild)
+	if (m_isSheild)// シールドが展開されている間
 	{
-		// シールド用のスケール行列を適用
-		Matrix shieldWorld = Matrix::CreateScale(m_sheildSize) * world;
-
-		// シールド
-		m_sheildModel->Draw(context, *states, shieldWorld, view, proj, false, [&]()
+		Matrix shieldWorld = Matrix::CreateScale(m_sheildSize) * world;// シールドのワールド行列
+		m_sheildModel->Draw(context, *states, shieldWorld, view, proj, false, [&]()// シールド描画
 			{
-				context->OMSetDepthStencilState(states->DepthRead(), 0);
-				context->RSSetState(states->CullClockwise());
-				context->OMSetBlendState(states->Additive(), nullptr, 0xffffffff);
+				context->OMSetDepthStencilState(states->DepthRead(), 0);// 深度ステンシルステート設定
+				context->RSSetState(states->CullClockwise());// ラスタライザーステート設定
+				context->OMSetBlendState(states->Additive(), nullptr, 0xffffffff);// ブレンドステート設定
 			});
 	}
-
-	// パーティクル描画
-	if (m_isParticle)
+	if (m_isParticle)// パーティクル再生フラグが立っている間
 	{
-		m_pParticle->CreateBillboard(m_pBoss->GetCameraTarget(), m_pBoss->GetCameraEye(), m_pBoss->GetCameraUp());
-		m_pParticle->Render(view, proj);
+		m_pParticle->CreateBillboard(m_pBoss->GetCameraTarget(), m_pBoss->GetCameraEye(), m_pBoss->GetCameraUp());// ビルボード作成
+		m_pParticle->Render(view, proj);// パーティクル描画
 	}
 }
--- Signal_Raiders/Game/Enemy/Boss/BossSheild/BossSheild.h
+++ Signal_Raiders/Game/Enemy/Boss/BossSheild/BossSheild.h
@@ -5,51 +5,67 @@
 #pragma once
 #ifndef BOSS_SHEILD_DEFINED
 #define BOSS_SHEILD_DEFINED
+// 標準ライブラリ
+#include <SimpleMath.h>
+// 外部ライブラリ
 #include "Game/CommonResources.h"
+#include "DeviceResources.h"
+#include "Libraries/Microsoft/ReadData.h"
+// 自作ヘッダーファイル
+#include "Game/KumachiLib/KumachiLib.h"
+#include "Game/Enemy/Parameters/EnemyParameters.h"
 #include "Game/Interface/IState.h"
 #include "Game/Particle/Particle.h"
 #include "Game/Enemy/Boss/Boss.h"
+
+// クラスの前方宣言
 class Boss;
 class Player;
 class CommonResources;
 class IState;
+
 class BossSheild
 {
+public:
+	// アクセサ
+	bool GetSheild() const { return m_isSheild; }
+	void SetSheild(bool isSheild) { m_isSheild = isSheild; }
+	int GetSheildHP() const { return m_sheildHP; }
+	void SetSheildHP(int sheildHP) { m_sheildHP = sheildHP; }
+	void SetPosition(DirectX::SimpleMath::Vector3 pos) { m_sheildPosition = pos; }
+	void SetRotation(DirectX::SimpleMath::Quaternion rot) { m_sheildRotation = rot; }
+public:
+	// public関数
+	BossSheild(int sheildHP, Boss* pBoss);// コンストラクタ
+	~BossSheild();// デストラクタ
+	void Initialize(CommonResources* resources);// 初期化
+	void Update(float elapsedTime);// 更新
+	void Render(ID3D11DeviceContext1* context,// 描画
+		DirectX::DX11::CommonStates* states,
+		DirectX::SimpleMath::Matrix world,
+		DirectX::SimpleMath::Matrix view,
+		DirectX::SimpleMath::Matrix proj);
 private:
 	// 共通リソース
 	CommonResources* m_commonResources;
 	// シールドモデル
 	std::unique_ptr < DirectX::Model> m_sheildModel;
-	std::unique_ptr<Particle> m_pParticle;// パーティクル
-	Boss* m_pBoss;// ボス
-	DirectX::SimpleMath::Vector3 m_sheildSize;// シールドのサイズ
-	DirectX::SimpleMath::Vector3 m_sheildPosition;// シールドの座標
-	DirectX::SimpleMath::Quaternion m_sheildRotation;// シールドの回転
+	// パーティクル
+	std::unique_ptr<Particle> m_pParticle;
+	// ボス
+	Boss* m_pBoss;
+	// シールドのサイズ
+	DirectX::SimpleMath::Vector3 m_sheildSize;
+	// シールドの座標
+	DirectX::SimpleMath::Vector3 m_sheildPosition;
+	// シールドの回転
+	DirectX::SimpleMath::Quaternion m_sheildRotation;
 	// シールド展開フラグ
 	bool m_isSheild;
 	// パーティクル再生可能フラグ
 	bool m_isParticle;
 	// シールドのHP
 	int m_sheildHP;
-public:
-	// 初期ステータスを設定
-	BossSheild(int sheildHP, Boss* pBoss);
-	~BossSheild();
-	void Initialize(CommonResources* resources);
-	void Update(float elapsedTime);
-	void Render(ID3D11DeviceContext1* context,
-		DirectX::DX11::CommonStates* states,
-		DirectX::SimpleMath::Matrix world,
-		DirectX::SimpleMath::Matrix view,
-		DirectX::SimpleMath::Matrix proj);
-public:
-	// getter
-	bool GetSheild() const { return m_isSheild; }
-	int GetSheildHP() const { return m_sheildHP; }
-	// setter
-	void SetSheildHP(int sheildHP) { m_sheildHP = sheildHP; }
-	void SetSheild(bool isSheild) { m_isSheild = isSheild; }
-	void SetPosition(DirectX::SimpleMath::Vector3 pos) { m_sheildPosition = pos; }
-	void SetRotation(DirectX::SimpleMath::Quaternion rot) { m_sheildRotation = rot; }
+
 };
 #endif //BOSS_SHEILD_DEFINED
