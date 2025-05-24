a99ee93dc7ec0e769892ab36c7d7baf773af1acd
Kumachan1123 <kumakei2004@gmail.com>
2025-05-25 03:54:23 +0900

    CommonResourcesのコメント書き直し

--- Signal_Raiders/Game/CommonResources.h
+++ Signal_Raiders/Game/CommonResources.h
@@ -3,6 +3,8 @@
 	@brief	シーンへ渡す、ゲーム内で使用する共通リソース
 */
 #pragma once
+// 標準ライブラリ
+#include <cassert>
 // 自作ヘッダー
 #include "Game/KumachiLib/AudioManager/AudioManager.h"
 
@@ -18,66 +20,38 @@ namespace mylib
 	class InputManager;
 }
 
-
-// 共通リソース
 class CommonResources
 {
-private:
-	// 受け渡しするリソース一覧
-	DX::StepTimer* m_stepTimer;
-
-	DX::DeviceResources* m_deviceResources;
-
-	DirectX::CommonStates* m_commonStates;
-
-	mylib::DebugString* m_debugString;
-
-	mylib::InputManager* m_inputManager;
-
-	AudioManager* m_audioManager;
-
-
-public:
-	CommonResources();
-	~CommonResources() = default;
-
-	void Initialize(
-		DX::StepTimer* timer,
-		DX::DeviceResources* dr,
-		DirectX::CommonStates* commonStates,
-		mylib::DebugString* debugString,
-		mylib::InputManager* inputManager,
-		AudioManager* audioManager
+public:// アクセサ
+	DX::StepTimer* GetStepTimer() const { return m_stepTimer; }// タイマーを取得する
+	DX::DeviceResources* GetDeviceResources() const { return m_deviceResources; }// デバイスリソースを取得する
+	DirectX::CommonStates* GetCommonStates() const { return m_commonStates; }// コモンステートを取得する
+	mylib::DebugString* GetDebugString() const { return m_debugString; }// デバッグ文字列を取得する
+	mylib::InputManager* GetInputManager() const { return m_inputManager; }// 入力マネージャを取得する
+	AudioManager* GetAudioManager() const { return m_audioManager; }// オーディオマネージャを取得する
+public:// publicメソッド
+	CommonResources();// コンストラクタ
+	~CommonResources() = default;// デストラクタ defaultで定義
+	void Initialize(// 初期化する
+		DX::StepTimer* timer,// 受け渡しするタイマー
+		DX::DeviceResources* dr,// 受け渡しするデバイスリソース
+		DirectX::CommonStates* commonStates,// 受け渡しするコモンステート
+		mylib::DebugString* debugString,// 受け渡しするデバッグ文字列
+		mylib::InputManager* inputManager,// 受け渡しする入力マネージャ
+		AudioManager* audioManager// 受け渡しするオーディオマネージャ
 	);
-
-	// getter
-	DX::StepTimer* GetStepTimer() const
-	{
-		return m_stepTimer;
-	}
-
-	DX::DeviceResources* GetDeviceResources() const
-	{
-		return m_deviceResources;
-	}
-
-	DirectX::CommonStates* GetCommonStates() const
-	{
-		return m_commonStates;
-	}
-
-	mylib::DebugString* GetDebugString() const
-	{
-		return m_debugString;
-	}
-
-	mylib::InputManager* GetInputManager() const
-	{
-		return m_inputManager;
-	}
-
-	AudioManager* GetAudioManager() const
-	{
-		return m_audioManager;
-	}
+private:// privateメンバ変数
+	// 受け渡しするリソース一覧
+	// タイマー
+	DX::StepTimer* m_stepTimer;
+	// デバイスリソース
+	DX::DeviceResources* m_deviceResources;
+	// コモンステート
+	DirectX::CommonStates* m_commonStates;
+	// デバッグ文字列
+	mylib::DebugString* m_debugString;
+	// 入力マネージャ
+	mylib::InputManager* m_inputManager;
+	// オーディオマネージャ
+	AudioManager* m_audioManager;
 };
